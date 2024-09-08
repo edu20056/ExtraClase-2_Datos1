e0761da1204319d5fe5a20fb121d1bb8edbfd35d
edu20056 <165208122+edu20056@users.noreply.github.com>
2024-09-08 14:05:19 -0600

    myLibraryProblem1

---
+++ Problema1/myLibrary/ClassSuma.cs
@@ -0,0 +1,18 @@
+namespace myLibrary;
+
+public class Suma //Esta clase es solo una prueba 
+{
+    private int x;
+    private int y;
+
+    public Suma(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int Sumar_Valores()
+    {
+        return x + y;
+    }
+}
